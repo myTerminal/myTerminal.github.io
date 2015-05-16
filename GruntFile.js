b2ac88ddc4218c520a534016b5fa554042cb5224
myTerminal <team.terminal@gmail.com>
2015-05-16 16:41:50 +0530

    add infrastructure

---
+++ GruntFile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        less: {
+            css: {
+                files: [
+                    {
+                        src: ["src/styles/styles.less"],
+                        dest: "build/styles/styles.css"
+                    }
+                ]
+            }
+        },
+        watch: {
+            css: {
+                files: "src/styles/**/*.less",
+                tasks: ["less"]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.registerTask("default", ["build"]);
+    grunt.registerTask("build", ["less"]);
+    grunt.registerTask("develop", ["watch"]);
+};
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+[![Dependency Status](https://david-dm.org/myTerminal/myTerminal.github.io.svg)](https://david-dm.org/myTerminal/myTerminal.github.io/)
+[![devDependency Status](https://david-dm.org/myTerminal/myTerminal.github.io/dev-status.svg)](https://david-dm.org/myTerminal/myTerminal.github.io#info=devDependencies)
+[![peer Dependency Status](https://david-dm.org/myTerminal/myTerminal.github.io/peer-status.svg)](https://david-dm.org/myTerminal/myTerminal.github.io#info=peerDependencies)
+[![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)
+
 # myTerminal.github.io
+
 myTerminal's GitHub pages
